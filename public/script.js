3805e742a07f913a87a23681197099c1fea12a75
Joshua surico <surico.joshua@gmail.com>
2024-05-21 18:05:05 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -68,7 +68,7 @@ function toLocalTime(date) {
     console.log(date);
     // Convert to german frankfurt local time
     let formatted = date.toLocaleString('fr-FR', {
-        timeZone: 'fra1',
+        timeZone: 'Europe',
         weekday: 'long',
         day: 'numeric',
         month: 'long',
